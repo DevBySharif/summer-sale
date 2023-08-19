4a0a2e8f43f6f82f0ba0869ed4c2920b7d5fb58c
Sharif Obaidullah <sharifmdobaidullah@gmail.com>
2023-08-19 23:59:12 +0600

    added intraction with js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+    let total = 0;
+    function btnClickHandler(event){
+        const selectedItemContainer = document.getElementById('selected-product')
+        const productName = event.parentNode.parentNode.childNodes[3].innerText;
+        
+        const li = document.createElement('li');
+        li.innerText = productName;
+        selectedItemContainer.appendChild(li)
+
+        const productPrice = event.parentNode.parentNode.childNodes[5].innerText.split(' ')[0]
+        total = parseFloat(total) + parseFloat(productPrice);
+
+        const previousTotalPrice = document.getElementById('total-price');
+        const previousTotalPriceString = previousTotalPrice.innerText;
+        const newTotalPrice = total + parseFloat(previousTotalPriceString)
+        previousTotalPrice.innerText = newTotalPrice;
+    }
