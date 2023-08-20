7905fbe898ce088f1fdf52bdedfecac661af0a93
Sharif Obaidullah <sharifmdobaidullah@gmail.com>
2023-08-20 19:12:11 +0600

    added three section

--- js/index.js
+++ js/index.js
@@ -1,61 +1,59 @@
-    let total = 0;
-    function btnClickHandler(event){
-        const selectedItemContainer = document.getElementById('selected-product');
-        const productName = event.parentNode.parentNode.childNodes[3].innerText;
-        
-        const li = document.createElement('li');
-        li.innerText = productName;
-        selectedItemContainer.appendChild(li);
+let total = 0;
+function btnClickHandler(event) {
+    const selectedItemContainer = document.getElementById('selected-product');
+    const productName = event.parentNode.parentNode.childNodes[3].innerText;
 
-        const productPrice = event.parentNode.parentNode.childNodes[5].innerText.split(' ')[0]
-        total = parseFloat(total) + parseFloat(productPrice);
+    const li = document.createElement('li');
+    li.innerText = productName;
+    selectedItemContainer.appendChild(li);
 
-        const previousTotalPrice = document.getElementById('total-price');
-        const previousTotalPriceString = previousTotalPrice.innerText;
-        const newTotalPrice = total + parseFloat(previousTotalPriceString);
-        previousTotalPrice.innerText = newTotalPrice;
-        
-      
+    const productPrice = event.parentNode.parentNode.childNodes[5].innerText
+    total += parseFloat(productPrice)
 
-        // enable button
-        const btnPurchase = document.getElementById('btn-purchase');
-        const btnApply = document.getElementById('btn-apply')
-        if(newTotalPrice > 200){
-            btnApply.removeAttribute('disabled');
-            
-        }if(newTotalPrice > 0){
-            btnPurchase.removeAttribute('disabled');
-        }
+    const previousTotalPrice = document.getElementById('total-price');
+    previousTotalPrice.innerText = total;
 
-        btnApplyHandler(total);
-        return(total)
-    }
-    
-    // promo code apply
-    function btnApplyHandler(total){
-        const inputArea = document.getElementById('input-area');
-        const inputValue = inputArea.value;
-        let couponApplied = false
+    // enable button
+    const btnPurchase = document.getElementById('btn-purchase');
+    const btnApply = document.getElementById('btn-apply')
+    if (total > 200) {
+        btnApply.removeAttribute('disabled');
 
-        if(!couponApplied){
-            if(inputValue === 'SELL200'){
-                const discountedPrice = total * 0.2;
-                console.log(discountedPrice);
-            }
-        }
-        
+    } if (total > 0) {
+        btnPurchase.removeAttribute('disabled');
     }
 
+    btnApplyHandler(total);
+}
 
-    // promo code copy
+// promo code apply
 
-    function copyPromoCode() {
-        const promoCode = "SELL200"; // Promo code text
-        const tempInput = document.createElement("input");
-        document.body.appendChild(tempInput);
-        tempInput.value = promoCode;
-        tempInput.select();
-        document.execCommand("copy");
-        document.body.removeChild(tempInput);
-        alert("Promo code copied: " + promoCode);
+function btnApplyHandler() {
+    const couponInputArea = document.getElementById('input-area');
+    const inputValue = couponInputArea.value;
+
+
+    if (inputValue == 'SELL200') {
+        const discountAmount = (total * 0.2).toFixed(2);
+        const discountPriceInput = document.getElementById('discount-price')
+        discountPriceInput.innerText = discountAmount;
+        const discountedTotalPrice = document.getElementById('discounted-total-price');
+        discountedTotalPrice.innerText = total - discountAmount;
+        alert('Coupon Applied')
     }
+    couponInputArea.value = '';
+}
+
+
+// promo code text copy
+
+function copyPromoCode() {
+    const promoCode = "SELL200"; 
+    const tempInput = document.createElement("input");
+    document.body.appendChild(tempInput);
+    tempInput.value = promoCode;
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    alert("Promo code copied: " + promoCode);
+}
